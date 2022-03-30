fee5c5364dfbff02e6d0e0009f21116ae0ba8d9b
Jen <73888356+berryjen@users.noreply.github.com>
2022-03-30 21:32:03 +0800

    Github API incorporation

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ end
 get "/history" do
   erb :work
 end
+
+get '/repo' do
+    repos= [
+     { :name => "JS weather", :URL => "https://github.com/berryjen/SheCodes-weather-project"},
+     { :name => "react weather", :URL => "https://github.com/berryjen/weather-react"}
+      ]
+    erb :github, { :locals => { :name => "jen", :repos => repos} }
+end
+# figure out how to manipulate hard-coded data to look more fluid
+# copy lines 11-18
+# rename 'repo' 
+# use repos = Github::Repos.new :user => 'berryjen'
+get '/projects' do
+   projects = Github:: projects.new :user => 'berryjen', :name => 'URL'
+   projects.
