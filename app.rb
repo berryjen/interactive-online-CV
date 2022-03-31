a504648b04be6aeafac3da7930ca538df812ea65
Jen <73888356+berryjen@users.noreply.github.com>
2022-03-31 21:48:52 +0800

    linked Github repos (names&url) changed url case from upper to lower in views

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'github_api'
 
 get '/' do
   erb :index
@@ -10,8 +11,8 @@ end
 
 get '/repo' do
     repos= [
-     { :name => "JS weather", :URL => "https://github.com/berryjen/SheCodes-weather-project"},
-     { :name => "react weather", :URL => "https://github.com/berryjen/weather-react"}
+     { :name => "JS weather", :url => "https://github.com/berryjen/SheCodes-weather-project"},
+     { :name => "react weather", :url => "https://github.com/berryjen/weather-react"}
       ]
     erb :github, { :locals => { :name => "jen", :repos => repos} }
 end
@@ -20,5 +21,6 @@ end
 # rename 'repo' 
 # use repos = Github::Repos.new :user => 'berryjen'
 get '/projects' do
-   projects = Github:: projects.new :user => 'berryjen', :name => 'URL'
-   projects.
+  repos= Github.repos.list user: 'berryjen'
+  erb :github, { :locals => { :name => "jen", :repos => repos} }
+end
